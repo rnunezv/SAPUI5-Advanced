34bf69d6bd55e18e867db272027f8639890df903
rnunezv <rnunezv@gmail.com>
2021-04-28 12:56:26 +0000

    04_Elementos_Controladores_UI5

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,34 @@
+// @ts-nocheck
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.Employees.controller.MainView", {
-			onInit: function () {
+        return Controller.extend("logaligroup.Employees.controller.MainView", {
+            onInit: function () {
 
-			}
-		});
-	});
+            },
+
+            onValidate: function () {
+                var inputEmployee = this.getView().byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length == 6) {
+                    //inputEmployee.setDescription("OK");
+
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true);
+                }
+                else {
+                    //inputEmployee.setDescription("Not OK");
+
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);
+                }
+            }
+        });
+    });
